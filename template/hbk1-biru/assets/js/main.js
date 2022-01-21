775f3e734fb4b31934119167408bd6ca12d4a595
Asrulramadani <asrulramadani0711@gmail.com>
2022-01-21 20:05:06 +0800

    update theme

--- template/hbk1-biru/assets/js/main.js
+++ template/hbk1-biru/assets/js/main.js
@@ -1,163 +1,124 @@
 "use strict";
 
-// // Popup card music
-// window.onload = function () {
-//     // popup card
-//     const popupCard = document.querySelector(".popup-card");
-
-//     popupCard.classList.add("active")
-
-        
-//     const audio = document.querySelector("#audio")
-    
-//     const btnTidak = document.querySelector(".tidak");
-//     btnTidak.addEventListener("click", ()=>{
-//         popupCard.classList.remove("active")
-//     })
-
-//     const btnYa = document.querySelector(".ya");
-//     btnYa.addEventListener("click", ()=>{
-//         popupCard.classList.remove("active");
-//         audio.play();
-//     })
-
-
-//     // Animation on load
-//     const box = document.querySelectorAll(".box");
-
-//     box.forEach(e => {
-//             e.classList.add("active");
-//     })
-// }
-
-
-const audio = document.querySelector("#audio")
+const audio = document.querySelector("#audio");
 
 //onBtn buka undangan click
 const bukaUndangan = document.querySelector(".buka-undangan");
 
 bukaUndangan.addEventListener("click", function () {
-    this.parentElement.parentElement.classList.add("d-none")
+  this.parentElement.parentElement.classList.add("d-none");
 
-    // Animation
-    const box = document.querySelectorAll(".box");
+  // Animation
+  const box = document.querySelectorAll(".box");
 
-    box.forEach(e => {
-            e.classList.add("active");
-    })
+  box.forEach((e) => {
+    e.classList.add("active");
+  });
 
-    // audio
-    audio.play()
-})
+  // audio
+  audio.play();
+  
+});
 
 
 // icon musik
-
 const iconMusik = document.querySelector("#icon-musik");
 iconMusik.addEventListener("click", function () {
-    if(audio.play()) {
-        audio.pause()
-        this.classList.replace("bi-pause-fill", "bi-play-fill");
-    }
-    else if(audio.pause()) {
-        audio.pause()
-        this.classList.replace("bi-play-fill", "bi-pause-fill");
-        
-    }
-})
-
-
+  if (audio.play()) {
+    audio.pause();
+    this.classList.replace("bi-pause-fill", "bi-play-fill");
+  } else if (audio.pause()) {
+    audio.play();
+    this.classList.replace("bi-play-fill", "bi-pause-fill");
+  }
+});
 
 // Countdown
-const tglTujuan = new Date('December 30 2022 08:00:00').getTime();
+const tglTujuan = new Date("December 30 2022 08:00:00").getTime();
 
-const hitungMundur = setInterval(function() {
-    const tglSekarang =  new Date().getTime();
+const hitungMundur = setInterval(function () {
+  const tglSekarang = new Date().getTime();
 
-    const selisih = tglTujuan - tglSekarang;
-    const hari = Math.floor(selisih / (1000 * 60 * 60 * 24));
-    const jam = Math.floor(selisih % (1000 * 60 * 60 * 24) / (1000 * 60 *60));
-    const menit = Math.floor(selisih % (1000 * 60 * 60 )/ (1000 * 60));
-    const detik = Math.floor(selisih % (1000* 60) / 1000);
-    
-    
-    const txtHari = document.getElementById('hari');
-    txtHari.innerHTML = hari;
-    const txtJam = document.getElementById('jam');
-    txtJam.innerHTML = jam;
-    const txtMenit = document.getElementById('menit');
-    txtMenit.innerHTML = menit;
-    const txtDetik = document.getElementById('detik');
-    txtDetik.innerHTML = detik;
-    
-    if(jam < 10) {
-        txtJam.innerHTML = `0${jam}`;
-    }
-    if(menit < 10) {
-        txtMenit.innerHTML = `0${menit}`;
-    }
-    if(detik < 10) {
-        txtDetik.innerHTML = `0${detik}`;
-    }
+  const selisih = tglTujuan - tglSekarang;
+  const hari = Math.floor(selisih / (1000 * 60 * 60 * 24));
+  const jam = Math.floor((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const menit = Math.floor((selisih % (1000 * 60 * 60)) / (1000 * 60));
+  const detik = Math.floor((selisih % (1000 * 60)) / 1000);
 
-    if( selisih <= 0) {
-        const countdown = document.getElementById("countdown");
-        countdown.style.display = 'none';
-        // clearInterval(hitungMundur);
-    }
+  const txtHari = document.getElementById("hari");
+  txtHari.innerHTML = hari;
+  const txtJam = document.getElementById("jam");
+  txtJam.innerHTML = jam;
+  const txtMenit = document.getElementById("menit");
+  txtMenit.innerHTML = menit;
+  const txtDetik = document.getElementById("detik");
+  txtDetik.innerHTML = detik;
 
-}, 1000)
+  if (jam < 10) {
+    txtJam.innerHTML = `0${jam}`;
+  }
+  if (menit < 10) {
+    txtMenit.innerHTML = `0${menit}`;
+  }
+  if (detik < 10) {
+    txtDetik.innerHTML = `0${detik}`;
+  }
+
+  if (selisih <= 0) {
+    const countdown = document.getElementById("countdown");
+    countdown.style.display = "none";
+    // clearInterval(hitungMundur);
+  }
+}, 1000);
 
 // onscroll animation
-window.addEventListener("scroll", function(){
-    const box = document.querySelectorAll(".animate");
-    const screenPosition = window.innerHeight/.9;
+window.addEventListener("scroll", function () {
+  const box = document.querySelectorAll(".animate");
+  const screenPosition = window.innerHeight / 0.9;
 
-    box.forEach(e => {
-        let contentPosition = e.getBoundingClientRect().top;
+  box.forEach((e) => {
+    let contentPosition = e.getBoundingClientRect().top;
 
-        if(contentPosition <= screenPosition) {
-            e.classList.add("active");
-        }else {
-            e.classList.remove("active");
-        }
-    })
-})
-
+    if (contentPosition <= screenPosition) {
+      e.classList.add("active");
+    } else {
+      e.classList.remove("active");
+    }
+  });
+});
 
 // Form submit to spreedsheets
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbydtI759XNMYoMNPeMlKu5pcnslQxjesQ6KOzxOkiL3PShOgcXtrWZG6uzZuMpI9PMM/exec';
-const form = document.forms['my-form'];
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbydtI759XNMYoMNPeMlKu5pcnslQxjesQ6KOzxOkiL3PShOgcXtrWZG6uzZuMpI9PMM/exec";
+const form = document.forms["my-form"];
 
 const myForm = document.querySelector(".myForm");
 const myAlert = document.querySelector(".alert");
 const btnKirim = document.querySelector(".btnKirim");
 const btnloading = document.querySelector(".btnLoading");
 
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   btnKirim.classList.toggle("d-none");
   btnloading.classList.toggle("d-none");
 
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        console.log('Success!', response);
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
 
-        myAlert.classList.toggle("d-none");
-        btnKirim.classList.toggle("d-none");
-        btnloading.classList.toggle("d-none");
-        form.reset();
+      myAlert.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      btnloading.classList.toggle("d-none");
+      form.reset();
     })
-    .catch(error => console.error('Error!', error.message))
-})
-
+    .catch((error) => console.error("Error!", error.message));
+});
 
 // Close alert
-const closeBtn = document.querySelector('.close');
+const closeBtn = document.querySelector(".close");
 
-closeBtn.addEventListener('click', ()=>{
-    myAlert.classList.toggle('d-none')
-})
+closeBtn.addEventListener("click", () => {
+  myAlert.classList.toggle("d-none");
+});
