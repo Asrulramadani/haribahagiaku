3c7948db42930d376316e7ce1f1f9d2a40e53117
Asrulramadani <asrulramadani0711@gmail.com>
2022-01-29 21:38:07 +0800

    update script

--- template/hbk1-biru/assets/js/main.js
+++ template/hbk1-biru/assets/js/main.js
@@ -135,9 +135,9 @@ closeBtn.addEventListener("click", () => {
 // prevent inspect element
 
 document.onkeydown = function(e) { 
-//  if( e.keyCode == 123 ){ 
-//    return false; 
-//  } 
+  if( e.keyCode == 123 ){ 
+    return false; 
+  } 
   if(e.ctrlKey && e.shiftKey &&e.keyCode == 'I'.charCodeAt(0)){ 
     return false; 
   } 
